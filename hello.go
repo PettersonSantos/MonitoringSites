ad8d83e041e366c70191c3abdb5f01d7237db0b2
PettersonSantos <petterson.santos@outlook.com.br>
2021-07-18 02:39:12 -0300

    construindo a parte de monitoramento

--- hello.go
+++ hello.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
 
 	exibeIntroducao()
-	exibeMenu()
-	comando := leComando()
 
-	switch comando {
-	case 1:
-		fmt.Println("monitoring")
-	case 2:
-		fmt.Println("Logs")
-	case 0:
-		fmt.Println("Saindo do programa")
-		os.Exit(0)
-	default:
-		fmt.Println("Não conheço este comando")
-		os.Exit(-1)
+	for {
+		exibeMenu()
+		comando := leComando()
+
+		switch comando {
+		case 1:
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo Logs...")
+		case 0:
+			fmt.Println("Saindo do programa")
+			os.Exit(0)
+		default:
+			fmt.Println("Não conheço este comando")
+			os.Exit(-1)
+		}
 	}
 
 }
@@ -45,4 +49,18 @@ func leComando() int {
 	fmt.Println("O comando escolhido foi", comandoLido)
 
 	return comandoLido
+}
+
+func iniciarMonitoramento() {
+	fmt.Println("Monitorando...")
+	for {
+		site := "https://random-status-code.herokuapp.com/"
+		resp, _ := http.Get(site)
+
+		if resp.StatusCode == 200 {
+			fmt.Println("Site:", site, "foi carregado com sucesso.")
+		} else {
+			fmt.Println("Site:", site, "esta com problemas. Status:", resp.StatusCode)
+		}
+	}
 }
